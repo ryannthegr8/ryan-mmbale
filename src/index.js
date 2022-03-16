a68fa1ae7ff8e98c0daf03976a9c50ff54bd8e18
ryannthegr8 <mmbaleryan@gmail.com>
2022-03-16 13:17:44 +0300

    first-commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import  ReactDOM  from 'react-dom'
+
+//importing the app component
+import App from './App'
+
+//importing the CSS
+import './index.css'
+
+ReactDOM.render(<App/>, document.querySelector ('#root'));
