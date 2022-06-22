40d0520c63fd215fd2530c30f5110e12ceb1493f
ryannthegr8 <mmbaleryan@gmail.com>
2022-06-23 02:03:06 +0300

    done till at profile image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+          <h5>Hi I`m</h5>
+          <h1>Ryan Mmbale</h1>
+          <h5 className='text-light'>Software Developer</h5>
+          <CTA />
+
+          <div className='me'>
+              <img src={ME} alt='me' />
+          </div>
+      </div>
+
+    </header>
   )
 }
 
